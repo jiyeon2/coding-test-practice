3bc99cafce7faf2c44cebfb90bb30f4e6969a2c1
jiyeon lee <a1919361@gmail.com>
2020-09-10 11:24:34 +0900

    교집합, 합집합 구하는 부분을 다시 짜서 해결함

--- programmers/2_뉴스 클러스터링.py
+++ programmers/2_뉴스 클러스터링.py
@@ -1,45 +1,53 @@
-# 테스트케이스 4,7,9,10,11 통과 못함
+
+
+import math
 def solution(str1, str2):
-    answer = 0
-    set1 = getMultiSet(str1)
-    set2 = getMultiSet(str2)
-    
-    intersection = getIntersection(set1,set2)
-    union = getUnion(set1,set2)
-    
-    if len(set1) == 0 and len(set2) == 0:
-        answer = 1
-    else:
-        if len(intersection) == 0 or len(union) == 0:
-            answer = 0
-        else:
-            answer = len(intersection)/len(union)
+    s1 = str1.lower()
+    s2 = str2.lower()
+
+    # 두 문자열로부터 2개의 문자로만 이뤄진 다중집합 만듦
+    # string.isalpha() : 문자열이 알파벳으로만 이뤄져있는지 여부 판별    
+    set1 = []
+    for i in range(len(s1)-1):
+        if s1[i:i+2].isalpha():
+            set1.append(s1[i:i+2])
             
+    set2 = []
+    for i in range(len(s2)-1):
+        if s2[i:i+2].isalpha():
+            set2.append(s2[i:i+2])
     
-    # print('s1:',set1,' s2: ',set2)
-    # print(intersection, union, answer)
-    return int(answer*65536)
+    # 다중집합이 공집합일 경우 바로 리턴
+    if set1 == set2 == []:
+        return 65536
+            
+    # 두 다중집합의 교집합, 합집합을 구함
+    intersection = getIntersection(set1,set2)
+    union = getUnion(set1,set2,intersection)
+    answer = len(intersection) / len(union)
+    
+    return math.floor(answer*65536)
 
-def getMultiSet(str):
-    result = []
-    str = str.lower()
-    alphabet = 'abcedfghijklmnopqrstuvwxyz'
-    for i in range(len(str)-1):
-        if str[i] not in alphabet or str[i+1] not in alphabet: continue
-        result.append(str[i:i+2])
-    return result
-        
+# 교집합 구하기
+# 길이가 짧은 배열 a 기준으로 for루프를 돌려서
+# b에도 같은 요소가 있다면 b에서 해당 요소를 삭제하고 교집합 배열에 추가함
 def getIntersection(a,b):
-    result = [x for x in a if x in b]
-    return result
+    if len(a) > len(b):
+        a,b = b,a
+    copyA = a[:]
+    copyB = b[:]
+    intersection = []
+    for c in copyA:
+        if c in copyB:
+            copyB.remove(c)
+            intersection.append(c)
+    return intersection
 
-def getUnion(a,b):
-    copyA = [x for x in a]
-    copyB = [x for x in b]
-    intersection = getIntersection(a,b)
-    for x in intersection:
-        copyA.remove(x)
-        copyB.remove(x)
-    result = copyA + copyB + intersection
-    
-    return result
+# 합집합 구하기 
+# 합집합은 a+b-intersection
+def getUnion(a,b,intersection):
+    union = a[:]+b[:]
+    for c in intersection:
+        if c in union:
+            union.remove(c)
+    return union
