19f6b8cc9e60ab7e94e11754e8dcdbc89c72a292
jiyeon lee <a1919361@gmail.com>
2020-09-10 10:17:54 +0900

    삭제될 블록 탐색과 빈블록 채우는 코드 분리, 통과 못했던 테스트케이스 통과

--- programmers/프렌즈4블록.py
+++ programmers/프렌즈4블록.py
@@ -3,21 +3,27 @@ def solution(m, n, board):
     board = [list(line) for line in board]
     willBeDeleted = set()
     
+    def fillEmptyBlock():
+        # 빈칸 채우기
+        
+        # 삭제된 칸의 열만 추려냄
+        emptyCols = set([col for (row,col) in willBeDeleted])
+        for col in emptyCols:
+            for i in range(m-1,-1,-1):
+                if board[i][col] != '0': continue
+                    
+                notEmptyRow = -1
+                for k in range(i-1, -1, -1):
+                    if board[k][col] == '0': continue
+                    notEmptyRow = k
+                    break
+                    
+                if notEmptyRow == -1: break 
+                    
+                board[i][col], board[notEmptyRow][col] = board[notEmptyRow][col], board[i][col]
+
     def browse():
         # 좌표 i : m-2부터 0 까지, j : 0부터 n-1까지 탐색
-        # 빈칸 채우기
-        for i in range(m-2,-1,-1):
-            for j in range(n-1):
-                if board[i][j] != '0': continue
-                    
-                notEmptyIndex = -1
-                for k in range(i-1,-1,-1):
-                    if board[k][j] == '0': continue
-                    notEmptyIndex = k
-                    break
-                if notEmptyIndex != -1:
-                    board[i][j], board[notEmptyIndex][j] = board[notEmptyIndex][j],board[i][j]
-
         # 인접블록 확인
         for i in range(m-2,-1,-1):
             for j in range(n-1):
@@ -26,7 +32,7 @@ def solution(m, n, board):
                         willBeDeleted.add((x,y))
                         
     while True:
-        browse() # 보드 탐색하며 삭제될 블록 좌표 저장, 빈공간 채움
+        browse() # 보드 탐색하며 삭제될 블록 좌표 저장
         
         # 삭제될 블록 갯수가 0 이면 while문 종료
         deletedBlockCount = len(willBeDeleted)
@@ -35,12 +41,17 @@ def solution(m, n, board):
         # 삭제될 블록 개수를 합함
         count += deletedBlockCount
         
-        # 실제 보드에서 블록 삭제, 삭제될 블록이 저장된 배열을 초기화 함
+        # 실제 보드에서 블록 삭제
         for (x,y) in willBeDeleted:
             board[x][y] = '0'
+        
+        # 삭제될 블록이 있는 열만 탐색하며 빈 블록 채우기
+        fillEmptyBlock() 
+        
+        # 삭제될 블록이 저장된 배열을 초기화 함
         willBeDeleted.clear()
         deletedBlockCount = 0
+
         
-    
     
     return count
