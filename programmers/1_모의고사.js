ee27a2f420461529b12684ae227cf613306c4f77
jiyeon lee <a1919361@gmail.com>
2020-09-13 09:13:22 +0900

    설명 붙임

--- programmers/1_모의고사.js
+++ programmers/1_모의고사.js
@@ -3,19 +3,26 @@ function solution(answers) {
   const p1 = [1, 2, 3, 4, 5];
   const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
   const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  let count = [0, 0, 0];
+
+  let count = [0, 0, 0]; // 각 수포자 정답 개수 저장
+
+  // 수포자1의 답패턴 배열 길이는 5개이므로
+  // answers배열이 6개인 경우 첫번째 답패턴에 접근하기 위해
+  // 6번 문제 답을 체크할 때는 6 % 5 (i % p1.length) 번째 인덱스로 패턴배열에 접근함
   for (let i = 0; i < answers.length; i++) {
     if (answers[i] === p1[i % p1.length]) count[0]++;
     if (answers[i] === p2[i % p2.length]) count[1]++;
     if (answers[i] === p3[i % p3.length]) count[2]++;
   }
 
+  // 정답 개수 저장한 배열 count [5,0,0] 을
+  // {수포자 번호, 정답개수} 형태로 만들어서 정렬함
+  // count = [ { id: 1, value: 5 }, { id: 2, value: 0 }, { id: 3, value: 0 } ]
   count = count
     .map((value, index) => {
       return { id: index + 1, value };
     })
     .sort((a, b) => b.value - a.value);
-  // count = [ { id: 1, value: 5 }, { id: 2, value: 0 }, { id: 3, value: 0 } ]
 
   const maxVal = count[0].value;
   for (let i = 0; i < count.length; i++) {
--- programmers/2_소수찾기.js
+++ programmers/2_소수찾기.js
@@ -6,19 +6,26 @@
 // -> 여기 코드 복붙함 https://stackoverflow.com/questions/9960908/permutations-in-javascript?page=1&tab=votes#tab-top
 // 시간복잡도가 어떻게 된건지 모르겠는데 일단 통과했다
 
+// 1. 입력된 숫자로 만들 수 있는 모든 순열 만들기
+// 2. 순열의 최대값까지 존재하는 소수 찾기
+// 3. 만든 순열 배열을 돌면서 소수인지 아닌지 확인하기
+
 function solution(numbers) {
   let count = 0;
+
+  // 입력된 문자열을 배열형태로 바꿈
   let arrNum = numbers.split("");
 
-  var per = permutator(arrNum); // 순열구하기
+  // 숫자로 만들 수 있는 모든 순열구하기
+  var per = permutator(arrNum);
 
-  const maxVal = per.reduce(function (a, b) {
-    // 순열 중 최대값찾기
-    return Math.max(a, b);
-  });
+  // 만들어진 순열 중 최대값 찾기
+  const maxVal = per.reduce((a, b) => Math.max(a, b));
 
-  const primeNumList = getPrimeNumList(maxVal); // 최대값까지 소수배열 만들기
+  // 최대값 까지의 소수 배열 구하기
+  const primeNumList = getPrimeNumList(maxVal);
 
+  // 만들어진 순열을 for문으로 돌면서 소수배열에 있는지 없는지 확인함으로써 소수 판별..
   per.forEach((num) => {
     if (primeNumList.includes(num)) {
       count += 1;
@@ -26,25 +33,35 @@ function solution(numbers) {
   });
   return count;
 }
-console.log(solution("123"));
+// console.log(solution("123"));
 
+// 주어진 숫자배열로 만들 수 있는 순열 반환
 function permutator(inputArr) {
-  let result = [];
-  let used = Array(inputArr.length).fill(false);
+  let result = []; // 가능한 순열 저장
+  let used = Array(inputArr.length).fill(false); // 해당 숫자가 사용되었는지 여부 저장
 
+  // 순열 만드는 재귀함수
   const permute = (arr, output = []) => {
-    const num = +output.join("");
+    // console.log("permute함수 시작--------------, 입력된 output:", output);
+    const num = +output.join(""); // output 배열을 숫자형을 만듦
+
     if (!result.includes(num) && num > 1) {
+      // 1은 소수가 아니라 제외함
+      // console.log("만들어진 숫자 :", num);
       result.push(num);
     }
-    if (num.length == arr.length) return;
+    //주어진 배열로 만들 수 있는 최대 길이이면 함수 종료
+    if (num.length === arr.length) return;
+
     for (let i = 0; i < arr.length; i++) {
       if (used[i]) continue;
       used[i] = true;
       output.push(arr[i]);
+      // console.log("permute 재귀 이전 상태", { used, output });
       permute(arr, output);
       used[i] = false;
       output.pop();
+      // console.log("permute 재귀 빠져나온 후 상태", { used, output });
     }
   };
   permute(inputArr);
@@ -52,21 +69,19 @@ function permutator(inputArr) {
 }
 // console.log(permutator("123"));
 
+// 주어진 수까지의 소수배열 만드는 함수
 function getPrimeNumList(maxNum) {
   let result = [];
   let isPrime = Array(maxNum + 1).fill(1);
   for (let i = 2; i <= Math.sqrt(maxNum + 1); i++) {
-    if (isPrime[i] == 0) {
-      continue;
-    }
+    if (isPrime[i] === 0) continue;
     for (let j = i + i; j <= maxNum; j += i) {
       isPrime[j] = 0;
     }
   }
-  isPrime.forEach((value, idx) => {
-    if (idx >= 2 && value === 1) {
-      result.push(idx);
-    }
-  });
+  for (let i = 0; i < isPrime.length; i++) {
+    if (i >= 2 && isPrime[i] === 1) result.push(i);
+  }
   return result;
 }
+// console.log(getPrimeNumList(10));
