03a61d85783ca5555fd94a6fac7325e6cd9194b1
jiyeon lee <a1919361@gmail.com>
2020-09-18 23:06:37 +0900

    h-index풀긴 했음

--- programmers/2_H-index.js
+++ programmers/2_H-index.js
@@ -1,15 +1,16 @@
 function solution(citations) {
-  var answer = -1;
-  // First we order the values from the largest to the lowest value
+  // 인용횟수 내림차순으로 정렬
   let arr = citations.slice().sort((a, b) => b - a);
-  if (arr[0] === 0) return 0;
-  // Then, we look for the last position in which value is greater than or equal to the position (we call h this position)
-  for (let i = arr.length; i > -1; i--) {
-    if (i <= arr[i]) {
-      answer = i;
-      break;
+
+  // we look for the last position in which value is greater than or equal to the position (we call h this position)
+  // 인용횟수 : [6,5,3,1,0]
+  // 인덱스       1,2,3,4,5  == 각 인용횟수 이상 인용된 논문의 개수
+  for (let h = arr.length; h > 0; h--) {
+    let value = arr[h - 1];
+    if (value >= h) {
+      return h;
     }
   }
-
-  return answer + 1;
+  // [0,0,0]의 경우
+  return 0;
 }
